78cf4c7f90dff3d791f6fe9728b903a66874c456
valuestudy <kraemer.julia@protonmail.com>
2024-04-12 12:29:32 +0200

    Survey Preferences finished

--- otree_valuestudy/survey/__init__.py
+++ otree_valuestudy/survey/__init__.py
@@ -5,7 +5,7 @@ class C(BaseConstants):
     NAME_IN_URL = 'survey'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
-
+    
 
 class Subsession(BaseSubsession):
     pass
@@ -15,6 +15,14 @@ class Group(BaseGroup):
     pass
 
 
+# some variables 
+LIKERT_CHOICES = [
+        [1, 'Stimme gar nicht zu'],
+        [2, 'Stimme eher nicht zu'],
+        [3, 'Neutral'],
+        [4, 'Stimme eher zu'],
+        [5, 'Stimme voll zu'],
+    ]
 
 
 class Player(BasePlayer):
@@ -49,42 +57,69 @@ class Player(BasePlayer):
         blank=True,
     )
 
+
+# SURVEY Preferences.html
     question1 = models.IntegerField(
         label="Es ist wichtig für mich, meine Privatsphäre im Internet zu schützen.",
-        choices=[
-            [1, 'Stimme gar nicht zu'],
-            [2, 'Stimme eher nicht zu'],
-            [3, 'Neutral'],
-            [4, 'Stimme eher zu'],
-            [5, 'Stimme voll zu']
-        ],
+        choices=LIKERT_CHOICES,
         widget=widgets.RadioSelectHorizontal,
     )
 
     question2 = models.IntegerField(
         label="Wenn ich Websites besuche, versuche ich normalerweise, so wenig Daten wie möglich preiszugeben.",
-        choices=[
-            [1, 'Stimme gar nicht zu'],
-            [2, 'Stimme eher nicht zu'],
-            [3, 'Neutral'],
-            [4, 'Stimme eher zu'],
-            [5, 'Stimme voll zu']
-        ],
+        choices=LIKERT_CHOICES,
         widget=widgets.RadioSelectHorizontal,
     )
 
     question3 = models.IntegerField(
         label="Ich schätze personalisierte Werbung, die auf meinen vorherigen Online-Aktivitäten zugeschnitten sind.",
-        choices=[
-            [1, 'Stimme gar nicht zu'],
-            [2, 'Stimme eher nicht zu'],
-            [3, 'Neutral'],
-            [4, 'Stimme eher zu'],
-            [5, 'Stimme voll zu']
-        ],
+        choices=LIKERT_CHOICES,
         widget=widgets.RadioSelectHorizontal,
     )
 
+# SURVEY Preferences2.html
+
+    question4 = models.IntegerField(
+            label="...dass Online-Organisationen nicht sind, wer sie vorgeben zu sein?",
+            choices=LIKERT_CHOICES,
+            widget=widgets.RadioSelectHorizontal,
+        )
+
+    question5 = models.IntegerField(
+            label="...dass bei der Anmeldung oder bei Online-Einkäufen viele persönliche Informationen angeben werden müssen?",
+            choices=LIKERT_CHOICES,
+            widget=widgets.RadioSelectHorizontal,
+        )
+
+    question6 = models.IntegerField(
+            label="...dass Personen, die Sie nicht kennen, durch Ihre Online-Aktivitäten persönliche Informationen über Sie erhalten?",
+            choices=LIKERT_CHOICES,
+            widget=widgets.RadioSelectHorizontal,
+        )
+
+    question7 = models.IntegerField(
+            label="...über Online-Identitätsdiebstahl?",
+            choices=LIKERT_CHOICES,
+            widget=widgets.RadioSelectHorizontal,
+        )
+
+    question8 = models.IntegerField(
+            label="...dass eine Nachricht, die Sie jemandem online schicken, unberechtigterweise an andere weitergeleitet werden könnte?",
+            choices=LIKERT_CHOICES,
+            widget=widgets.RadioSelectHorizontal,
+        )
+
+    question9 = models.IntegerField(
+            label="...dass Informationen über Sie auf einem alten Computer gefunden werden könnten?",
+            choices=LIKERT_CHOICES,
+            widget=widgets.RadioSelectHorizontal,
+        )
+
+    question10 = models.IntegerField(
+            label="...dass Nachrichten, die Sie online erhalten, nicht von der Person stammen, die diese vorgibt zu sein?",
+            choices=LIKERT_CHOICES,
+            widget=widgets.RadioSelectHorizontal,
+        )
 
 # FUNCTIONS
 
@@ -99,5 +134,9 @@ class Preferences(Page):
     form_model = 'player'
     form_fields = ['question1', 'question2', 'question3']
 
+class Preferences2(Page):
+    form_model = 'player'
+    form_fields = ['question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10']
 
-page_sequence = [Demographics, Preferences ]
+
+page_sequence = [Demographics, Preferences, Preferences2 ]
