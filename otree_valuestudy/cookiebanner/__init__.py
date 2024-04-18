061403ca7fb76027275c9d9f99578480e530b132
valuestudy <kraemer.julia@protonmail.com>
2024-04-18 18:35:22 +0200

    Fixed sending button choice to backend

--- otree_valuestudy/cookiebanner/__init__.py
+++ otree_valuestudy/cookiebanner/__init__.py
@@ -25,11 +25,19 @@ class Player(BasePlayer):
     cookie_choice = models.StringField(
         label="Bitte wählen sie aus ob sie Cookies akzeptieren oder ablehnen wollen.",
         choices=[
-            ['1', "Alle Cookies Akzeptieren"],
-            ['2', "Alle Cookies Ablehnen"],
-            ['3', "Marke"]
-        ]
+            ["all-accepted", "Alle akzeptiert"],
+            ["all-declined", "Alle abgelehnt"]
+        ],
+
+        blank=True,
     
+    )
+
+    cookie_choice_settings = models.StringField(
+        label="Bitte wählen sie aus ob sie Cookies akzeptieren oder ablehnen wollen.",
+        
+
+        blank=True,
     )
 
     cookie_analytics = models.BooleanField(
@@ -38,7 +46,7 @@ class Player(BasePlayer):
             [True, "Analytische Cookies zugestimmt"],
             [False, "Analytische Cookies abgelehnt"]
         ],
-        widget=widgets.RadioSelectHorizontal,
+
         blank=True,
     )
 
@@ -67,11 +75,22 @@ class Player(BasePlayer):
 class Index(Page):
 
     form_model = 'player'
-    form_fields = [ 'cookie_analytics', 'cookie_comfort', 'cookie_marketing' ]
+    form_fields = [ 'cookie_choice', 'cookie_choice_settings', 'cookie_analytics', 'cookie_comfort', 'cookie_marketing' ]
 
-
+    @staticmethod
     def vars_for_template(self):
         return {}
+    
+    @staticmethod
+    def live_method(player: Player, data): #as it is not possible to send data via a button without submitting the form, the live method is used which sends data directly to the server on a click
+        if data == 'all-accepted':
+            player.cookie_choice = 'all-accepted'
+        if data == 'all-declined':
+            player.cookie_choice = 'all-declined'
+        if data == 'all-accepted-settings':
+            player.cookie_choice = 'all-accepted-settings'
+        if data == 'apply-settings':
+            player.cookie_choice = 'apply-settings'
 
 
 
